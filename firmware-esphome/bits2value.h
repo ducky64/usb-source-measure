b0d9f7843f71b80485a785e2b6edc1018e372419
Richard Lin <richard.lin@berkeley.edu>
2026-01-02 20:51:52 -0800

    refactor to use adc utils

---
+++ firmware-esphome/bits2value.h
@@ -0,0 +1,15 @@
+const float kVRef = 3.3;
+const float kVCenter = 1.65;
+
+// Given an ADC (or DAC) value and calibrations, returns the value in units (eg, volts).
+// Input should be a ratio (0-1)
+float adcToValue(float adc_ratio, float ratio, float factor, float offset) {
+    return adc_ratio * kVRef * ratio * factor + offset;
+}
+
+// Inverse of adcToValue.
+// Note, the current SMU architecture requires a DAC signal opposite (around the center)
+// of the measurement target, this inversion must happen elsewhere.
+float valueToAdc(float value, float ratio, float factor, float offset) {
+    return (value - offset) / factor / ratio / kVRef;
+}
