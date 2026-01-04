946306c5e84e5b532dd4f9fa76cab44d5e48085a
Richard Lin <elpato25@gmail.com>
2026-01-03 20:43:41 -0800

    LOL

--- firmware-esphome/custom_components/fusb302/__init__.py
+++ firmware-esphome/custom_components/fusb302/__init__.py
@@ -26,4 +26,5 @@ CONFIG_SCHEMA = (
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
     cg.add(var.set_target(config[CONF_TARGET_VOLTAGE]))
